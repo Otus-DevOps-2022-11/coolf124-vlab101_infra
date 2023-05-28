d0c36ff422e6bbceb1ad6e4a4877f238ece4f2fe
user <coolf124@gmail.com>
2023-05-28 20:43:43 +0300

    add files to main dir terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,42 +4,16 @@ provider "yandex" {
   folder_id                = var.folder_id
   zone                     = var.zone
 }
-resource "yandex_compute_instance" "app" {
-  count = var.reddit_app_instance_count
-  name  = "reddit-app-${count.index + 1}"
-  resources {
-    cores  = 2
-    memory = 2
-  }
+module "app" {
+  source          = "./modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
+}
 
-  boot_disk {
-    initialize_params {
-      # Указать id образа созданного в предыдущем домашем задании
-      image_id = var.image_id
-    }
-  }
-
-  network_interface {
-    # Указан id подсети default-ru-central1-a
-    subnet_id = var.subnet_id
-    nat       = true
-  }
-  metadata = {
-    ssh-keys = "ubuntu:${file(var.public_key_path)}"
-  }
-  connection {
-    type  = "ssh"
-    host  = self.network_interface.0.nat_ip_address
-    user  = "ubuntu"
-    agent = false
-    # путь до приватного ключа
-    private_key = file("~/.ssh/appuser")
-  }
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
+module "db" {
+  source          = "./modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
 }
